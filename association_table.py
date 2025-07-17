85bcbf96b159b6546fabe2cde95b195ce4d4c413
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-07-17 21:07:02 +0300

    Initial import
    Bi-Directional Many-to-many (association table)

---
+++ association_table.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from __future__ import annotations
+
+from typing import List
+from sqlalchemy import Column, Table, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+association_table = Table(
+    "association_table",
+    Base.metadata,
+    Column("left_id", ForeignKey("left_table.id"), primary_key=True),
+    Column("right_id", ForeignKey("right_table.id"), primary_key=True),
+)
+
+
+class Parent(Base):
+    __tablename__ = "left_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    children: Mapped[List[Child]] = relationship(
+        secondary=association_table, back_populates="parents"
+    )
+
+
+class Child(Base):
+    __tablename__ = "right_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    parents: Mapped[List[Parent]] = relationship(
+        secondary=association_table, back_populates="children"
+    )

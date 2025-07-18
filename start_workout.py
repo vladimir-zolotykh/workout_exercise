cee81a2bf3be5c5d91cbc3334b27ce8d90cb25bd
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-07-18 21:20:28 +0300

    Add abbreviated_input()
    Refactor __main__

--- start_workout.py
+++ start_workout.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, field
 from datetime import datetime
 import pprint
 
-exercises = ["squat", "bench press", "deadlift"]
+exercises: List[str] = ["squat", "bench press", "deadlift"]
 
 
 class ExerciseNameError(Exception):
@@ -22,6 +22,7 @@ class Exercise:
 
 @dataclass
 class Workout:
+    name: str
     started: datetime
     exercises: List[Exercise] = field(default_factory=list)
 
@@ -31,19 +32,41 @@ class Logbook:
     workouts: List[Workout] = field(default_factory=list)
 
 
+def abbreviated_input(prompt: str, choices: List[str] = exercises + ["quit"]) -> str:
+    while True:
+        user_input = input(f"{prompt} [{', '.join(choices)}] ?")
+        matches = [
+            choice for choice in choices if choice.lower().startswith(user_input)
+        ]
+        if len(matches) == 1:
+            return matches[0]
+        elif len(matches) == 0:
+            print("No match found. Try again.")
+        else:
+            print(f"Ambiguous input. It matches: {', '.join(matches)}. Try again.")
+
+
 if __name__ == "__main__":
+    workout_id: int = 0
     logbook: Logbook = Logbook()
-    name: str
-    while name := input("exercise name? "):
-        workout: Workout
-        if name not in exercises + ["quit"]:
-            raise ExerciseNameError(name)
-        if name == "quit":
+    workout_name: str
+    while workout_name := input("workout name? "):
+        if workout_name == "quit":
             break
-        weight: float = float(input("weight? "))
-        reps: int = int(input("reps? "))
-        exercise: Exercise = Exercise(name, weight, reps)
-        workout = Workout(datetime.now())
-        workout.exercises.append(exercise)
+        workout: Workout
+        if workout_name == "":
+            workout_id += 1
+            workout_name = f"workout#{workout_id}"
+        workout = Workout(name=workout_name, started=datetime.now())
+        exercise_name: str
+        while exercise_name := abbreviated_input("exercise name"):
+            if exercise_name not in exercises + ["quit"]:
+                raise ExerciseNameError(exercise_name)
+            if exercise_name == "quit":
+                break
+            weight: float = float(input("weight? "))
+            reps: int = int(input("reps? "))
+            exercise: Exercise = Exercise(exercise_name, weight, reps)
+            workout.exercises.append(exercise)
         logbook.workouts.append(workout)
     pprint.pprint(logbook)
